2c714342f86eeca1fd924ee7fc5b4130bf98102e
Timea Bartha <timea.bartha.93@gmail.com>
2024-08-22 16:25:30 +0200

    Initial Commit

---
+++ CheddarWipeout/CheddarWipeoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheddarWipeoutApp.swift
+//  CheddarWipeout
+//
+//  Created by Timea Bartha on 22/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CheddarWipeoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
